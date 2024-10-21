7caffe5b246d15db1f3e23a5f4384d3752f9c53d
sirot999 <55985247+sirot999@users.noreply.github.com>
2024-10-21 12:01:43 +0200

    Add Clustering Node (#4)
    * Initial clustering node class.
    
    * Updated clustering node implementation and added new config, launch, CMakeLists and package files
    
    * Updated CMakeLists and package
    
    * Updated clustering.hpp and clustering.cpp
    
    * Updated node methods, plus general refactoring. Created yaml config file. Created launch file.
    
    * Updated publish of centroids vector and general comments refactoring in clustering.cpp
    
    * Bug fixes: fixed swap of cluster size parameters. Added missing includes and missing class members.
    Minor refactoring.
    
    * Launch file update: added static_transform_publisher node to the launch config.
    
    * Documentation refactoring and minor changes.
    
    * swapped tabs with white spaces
    
    ---------
    
    Co-authored-by: matteogianferrari <mine.matt88@gmail.com>
    Co-authored-by: Pietro Moriello <284427@studenti.unimore.it>

---
+++ clustering_proc/include/clustering_proc/clustering.hpp
@@ -0,0 +1,137 @@
+/**
+ * @file    clustering.hpp
+ * 
+ * @author  Lorenzo Sirotti
+ *
+ * @date    2024-10-11
+ * 
+ * @brief   Header file for Clustering node.
+ */
+#ifndef CLUSTERING_HPP_
+#define CLUSTERING_HPP_
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include <vector>
+
+/**
+ * @class   ClusteringNode
+ *
+ * @brief   ROS2 node that performs point cloud clustering using a 2D Lidar scan.
+ * 
+ * @details This node converts a 2D Lidar scan into a point cloud,
+ *          then performs clustering to detect objects.
+ */
+class ClusteringNode : public rclcpp::Node
+{
+public:
+    /**
+     * @fn    ClusteringNode
+     * 
+     * @brief Constructs a new ClusteringNode object.
+     */
+    ClusteringNode();
+
+private:
+    /**
+     * @fn          laserScanCallback
+     * 
+     * @brief       Callback function for receiving and processing Lidar scan data.
+     *
+     * @details     Converts the 2D Lidar scan to a 3D point cloud, filters it,
+     *              and applies clustering to detect objects.
+     * 
+     * @param[in]   msg LaserScan message containing the Lidar data.
+     */
+    void laserScanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+
+    /**
+     * @fn          readLidarScan
+     * 
+     * @brief       Converts a 2D Lidar scan into a 3D point cloud.
+     *
+     * @details     Each point in the Lidar scan is projected into 3D space
+     *              by computing its X and Y coordinates using polar coordinates,
+     *              while the Z coordinate is set to zero.
+     * 
+     * @param[in]   scan LaserScan message containing the Lidar data.
+     * @param[out]  outputPC Pointer to the output 3D point cloud.
+     */
+    void readLidarScan(const sensor_msgs::msg::LaserScan::SharedPtr scan, pcl::PointCloud<pcl::PointXYZ>::Ptr& outputPC);
+
+    /**
+     * @fn          reduceDensityPC
+     *
+     * @brief       Reduces the density of the input point cloud using a VoxelGrid filter.
+     * 
+     * @details     Downsamples the point cloud to reduce the number of points,
+     *              improving processing speed without significantly losing geometric information.
+     *
+     * @param[in]   inputPC Pointer to the input 3D point cloud.
+     * @param[out]  outputPC Pointer to the output 3D point cloud.
+     */
+    void reduceDensityPC(pcl::PointCloud<pcl::PointXYZ>::Ptr& inputPC, pcl::PointCloud<pcl::PointXYZ>::Ptr& outputPC);
+
+    /**
+     * @fn          cropFOVPC
+     *
+     * @brief       Crops the input point cloud to focus on the region of interest.
+     * 
+     * @details     Applies a CropBox filter to remove points outside the desired field of view,
+     *              defined by the min and max points in 3D space.
+     *
+     * @param[in]   inputPC Pointer to the input 3D point cloud.
+     * @param[out]  outputPC Pointer to the output 3D point cloud.
+     */
+    void cropFOVPC(pcl::PointCloud<pcl::PointXYZ>::Ptr& inputPC, pcl::PointCloud<pcl::PointXYZ>::Ptr& outputPC);
+
+    /**
+     * @fn          applyClustering
+     *
+     * @brief       Applies Euclidean Clustering to group points into distinct clusters.
+     * 
+     * @details     Segments the point cloud into clusters based on the Euclidean distance between points.
+     *              Each cluster represents a potential object in the scene.
+     *
+     * @param[in]   inputPC Pointer to the input 3D point cloud.
+     *
+     * @return      std::vector<Eigen::Vector4f> Vector of centroids.
+     */
+    std::vector<Eigen::Vector4f> applyClustering(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputPC);
+
+
+    /**
+     * @brief   Euclidean Clustering object for detecting point cloud clusters.
+     */
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> euclideanClustering_;    
+    pcl::VoxelGrid<pcl::PointXYZ> voxelGrid_;	/**< VoxelGrid filter for downsampling the point cloud.*/
+    pcl::CropBox<pcl::PointXYZ> cropBox_;		/**< CropBox filter for cropping the point cloud.*/
+
+    /**
+     * @brief   Subscriber for lidar scan messages.
+     */
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_scan_subscription_;
+    /**
+     * @brief   Publisher for computed cluster centroids.
+     */
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr centroid_publisher_;
+
+    std::vector<double> voxelConf_; /**< VoxelGrid leaf sizes for downsampling in X, Y, and Z axes.*/
+
+    Eigen::Vector4f cb_minPoint_;   /**< Minimum point for the CropBox filter.*/
+    Eigen::Vector4f cb_maxPoint_;   /**< Maximum point for the CropBox filter.*/
+
+    double ecTolerance_;    /**< Tolerance for Euclidean Clustering (maximum distance between points in a cluster).*/
+    size_t minClusterSize_; /**< Minimum number of points required to form a cluster.*/
+    size_t maxClusterSize_; /**< Maximum number of points allowed in a cluster.*/
+};
+
+#endif  // CLUSTERING_HPP_
---
+++ clustering_proc/launch/clustering_launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution, PythonExpression
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+
+# to be used with the OpaqueFunction action. approach needed to obtain the substitution of 'transform_config', cast it to a list and concatenate other arguments
+def setup_launch(context, *args, **kwargs):
+
+    config = PathJoinSubstitution([FindPackageShare('clustering_proc'), 'config', 'clustering_node.yaml'])
+    # define launch configuration to store the transform argument for the static_transform node
+    # LaunchConfiguration substitutions allow us to acquire the value of the launch argument in any part of the launch description.
+    transform_config = LaunchConfiguration('transform')
+    
+    clustering = Node(
+        package='clustering_proc',
+        executable='clustering',
+        name='scan_clustering',
+        parameters=[config]
+    )
+    # transform_config argument will be returned as a string here
+    tf_args = transform_config.perform(context).split() + ['base_link', 'lidar_link']
+    static_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='lidar_to_base_static_transform_publisher',
+        arguments = tf_args
+    )
+    
+    return [clustering, static_transform]
+
+def generate_launch_description():
+    # define launch argument that can be passed form the console (or above launch files)
+    transform_config_launch_arg = DeclareLaunchArgument(
+        'transform',
+        default_value='0 0 0 0 0 0',
+        description='Static transform from lidar_link to base_link. Format: x y z roll pitch yaw'
+    )
+    return LaunchDescription([
+        transform_config_launch_arg,
+        OpaqueFunction(function=setup_launch)
+    ])
