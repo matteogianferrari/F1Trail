b11b262b52f15eeb0aeef731ba648d9d5725db77
matteogianferrari <mine.matt88@gmail.com>
2024-10-21 15:45:51 +0200

    Added subscribers and publishers, added debug messages to test the node.

--- tracking_proc/src/tracker.cpp
+++ tracking_proc/src/tracker.cpp
@@ -16,41 +16,67 @@ TrackerNode::TrackerNode(): Node("tracking_module") {
     this->declare_parameter("cluster_centroids_topic", "/cluster_centroids");
     std::string cluster_centroids_topic = this->get_parameter("cluster_centroids_topic").as_string();
     
-
+    // Defines quality of service: all messages that you want to receive must have the same
+    rclcpp::QoS custom_qos_profile = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default))
+    .history(rmw_qos_history_policy_t::RMW_QOS_POLICY_HISTORY_KEEP_LAST)
+    .keep_last(10)
+    .reliability(rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
+    .durability(rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE)
+    .avoid_ros_namespace_conventions(false);
+    
     // Subscribes to target topic
-    subTargetLoc_.subscribe(this, target_location_topic, custom_qos_profile.get_rmw_qos_profile());
+    subTargetLoc_ = this->create_subscription<geometry_msgs::msg::Point>(
+        target_location_topic, custom_qos_profile, std::bind(&TrackerNode::target_callback, this, std::placeholders::_1));
 
     // Subscribes to the clustering topic
-    subClusterCentroids_.subscribe(this, cluster_centroids_topic, custom_qos_profile.get_rmw_qos_profile());
+    subClusterCentroids_ = this->create_subscription<geometry_msgs::msg::PoseArray>(
+        cluster_centroids_topic, custom_qos_profile, std::bind(&TrackerNode::centroids_callback, this, std::placeholders::_1));
 
     // create publisher for target location point
-    pub_ = this->create_publisher<geometry_msgs::msg::Point>("/tracked_obj_loc", 10);
+    pub_ = this->create_publisher<geometry_msgs::msg::Point>("/tracked_obj_loc", custom_qos_profile);
 }
 
 
 void TrackerNode::target_callback(const geometry_msgs::msg::Point::SharedPtr stereo_msg) {
     RCLCPP_INFO(this->get_logger(), "Received target location from aruco_loc node.");
 
+    // Stores the target location received from the Aruco node
     currAlpha_ = pointToEigen(stereo_msg);
 
-    if () {
-        return;
-    }
+    // Print the target location (assuming currAlpha_ is an Eigen vector)
+    RCLCPP_INFO(this->get_logger(), "Target Location: [x: %f, y: %f, z: %f]", 
+                currAlpha_[0], currAlpha_[1], currAlpha_[2]);
 
-    trackerCore();
+    // Edge case: No centroids have been received yet
+    // if () {
+    //     return;
+    // }
+
+    // Tracks the object
+    // trackerCore();
 }
 
 
 void TrackerNode::centroids_callback(const geometry_msgs::msg::PoseArray::SharedPtr cluster_msg) {
     RCLCPP_INFO(this->get_logger(), "Received clusters centroids from clustering node.");
 
+    // Stores the centroids received from the Clustering node
     centroids_ = poseArrayToEigen(cluster_msg);
 
-    if () {
-        return;
+    // Print the centroids (assuming centroids_ is a vector of Eigen vectors)
+    RCLCPP_INFO(this->get_logger(), "Centroids:");
+    for (size_t i = 0; i < centroids_.size(); ++i) {
+        RCLCPP_INFO(this->get_logger(), "Centroid %zu: [x: %f, y: %f, z: %f]", 
+                    i, centroids_[i][0], centroids_[i][1], centroids_[i][2]);
     }
 
-    trackerCore();
+    // Edge case: No target location has been received yet
+    // if () {
+    //     return;
+    // }
+
+    // Tracks the object
+    // trackerCore();
 }
 
 
@@ -114,7 +140,7 @@ std::vector<Eigen::VectorXd> TrackerNode::poseArrayToEigen(const geometry_msgs::
 int main(int argc, char ** argv) {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<Tracker>();
+    auto node = std::make_shared<TrackerNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
 
