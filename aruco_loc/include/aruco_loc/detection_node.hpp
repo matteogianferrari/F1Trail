aeadf2cf8fbd599b0fc460b5e1937831f6295a02
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2024-10-25 17:03:33 +0200

    Updated Vision Nodes (#6)
    * Adding vision nodes: sl_stereo_proc for processing concatenated stereo images coming from a StreoLabs Zed camera; aruco_loc for localization of ArUco markers using stereo images.
    
    * Comment refactoring.
    
    * Change message type pulished to PointStamped.
    
    * Changed launch config: added static transform publisher from camera_link to base_link
    
    * Change of reference frame for computed location to center of the left pinhole camera.
    Added two parameters to aruco_loc node (x and y center translation).
    Change measure unit of published location from centimeters to meters.
    
    * Update launch file.
    
    Modified description of launch argument for static transform publisher.
    
    * Corrected computation of location point. Converted tabs to spaces.
    
    * Modified QoS of /target_loc publisher.
    
    ---------
    
    Co-authored-by: Pietro Moriello <284427@studenti.unimore.it>

--- aruco_loc/include/aruco_loc/detection_node.hpp
+++ aruco_loc/include/aruco_loc/detection_node.hpp
@@ -11,7 +11,7 @@
 #define DETECTION_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "std_msgs/msg/header.hpp"
 #include <cv_bridge/cv_bridge.h>
@@ -37,74 +37,75 @@
 class ImageDetectionNode : public rclcpp::Node
 {
 public:
-	/**
-	* @fn     ImageDetectionNode
-	* 
-	* @brief  Constructs a new ImageDetectionNode object.
-	*/
+    /**
+    * @fn     ImageDetectionNode
+    * 
+    * @brief  Constructs a new ImageDetectionNode object.
+    */
     ImageDetectionNode();
 
 private:
-	/**
-	 * @fn 			exact_sync_callback
-	 * 
-	 * @brief		Callback function for synchronizing stereo images.
-	 *
-	 * @details 	This callback is called when two images with the same timestamp
-	 *				are published on their respective topics.
-	 * 
-	 * @param[in]	left_img Image message for left stereo component.
-	 * @param[in] 	right_img Image message for right stereo component.
-	 */
-	void exact_sync_callback(const sensor_msgs::msg::Image::ConstSharedPtr left_img, const sensor_msgs::msg::Image::ConstSharedPtr right_img);
+    /**
+     * @fn 			exact_sync_callback
+     * 
+     * @brief		Callback function for synchronizing stereo images.
+     *
+     * @details 	This callback is called when two images with the same timestamp
+     *				are published on their respective topics.
+     * 
+     * @param[in]	left_img Image message for left stereo component.
+     * @param[in] 	right_img Image message for right stereo component.
+     */
+    void exact_sync_callback(const sensor_msgs::msg::Image::ConstSharedPtr left_img, const sensor_msgs::msg::Image::ConstSharedPtr right_img);
 
-	/**
-	 * @fn			stereo_target_detect
-	 *
-	 * @brief 		Target detection.
-	 * 
-	 * @details		Detection is performed with opencv's ArUco marker library.
-	 *				The marker needs to be detected in both images.
-	 *				The projected center of the marker is found for each image
-	 *				and it is used to compute the disparity.
-	 *
-	 * @param[in]	left_img cv_bridge wrap of the received left image message.
-	 * @param[in] 	right_img cv_bridge wrap of the received right image message.
-	 */
-	void stereo_target_detect(cv_bridge::CvImageConstPtr left_img, cv_bridge::CvImageConstPtr right_img);
+    /**
+     * @fn			stereo_target_detect
+     *
+     * @brief 		Target detection.
+     * 
+     * @details		Detection is performed with opencv's ArUco marker library.
+     *				The marker needs to be detected in both images.
+     *				The projected center of the marker is found for each image
+     *				and it is used to compute the disparity.
+     *
+     * @param[in]	left_img cv_bridge wrap of the received left image message.
+     * @param[in] 	right_img cv_bridge wrap of the received right image message.
+     */
+    void stereo_target_detect(cv_bridge::CvImageConstPtr left_img, cv_bridge::CvImageConstPtr right_img);
 
-	/**
-	 * @fn 			target_center
-	 *
-	 * @brief 		Computes target's center.
-	 * 
-	 * @details		The center is computed as the mean of corners' coordinates.
-	 *
-	 * @param[in] 	corners Image plane pixel coordinates of detected marker's corners
-	 *
-	 * @return 		Target center in image plane pixel coordinates.
-	 */
+    /**
+     * @fn 			target_center
+     *
+     * @brief 		Computes target's center.
+     * 
+     * @details		The center is computed as the mean of corners' coordinates.
+     *
+     * @param[in] 	corners Image plane pixel coordinates of detected marker's corners
+     *
+     * @return 		Target center in image plane pixel coordinates.
+     */
     cv::Point2f target_center(std::vector<cv::Point2f> const &corners) const;
 
 
-	/**
-	 * @brief	Synchronizes left and right image topics based on exact matching of timestamps.
-	 */
-	std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Image,sensor_msgs::msg::Image>> sync_exact_;
-	rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr pub_;			/**< Publisher of detected target location.*/
-	message_filters::Subscriber<sensor_msgs::msg::Image> sub_right_img_;	/**< Receives the right image stream.*/
-	message_filters::Subscriber<sensor_msgs::msg::Image> sub_left_img_;		/**< Receives the left image stream.*/
-	
-	/**
-	 * @brief	ArUco dictionary of markers to be used.
-	 *			See OpenCV's documentation on corresponding enum entries.
-	 */
-	cv::aruco::PredefinedDictionaryType aruco_dict_;
-	cv::aruco::ArucoDetector detector_;		/**< ArUco marker detector.*/
-	
-	int marker_idx_;	/**< Index of ArUco marker to detect.*/
-	double B;			/**< Horizontal displacement of stereo cameras (assume no rotations).*/
+    /**
+     * @brief	Synchronizes left and right image topics based on exact matching of timestamps.
+     */
+    std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Image,sensor_msgs::msg::Image>> sync_exact_;
+    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr pub_;	/**< Publisher of detected target location.*/
+    message_filters::Subscriber<sensor_msgs::msg::Image> sub_right_img_;	/**< Receives the right image stream.*/
+    message_filters::Subscriber<sensor_msgs::msg::Image> sub_left_img_;		/**< Receives the left image stream.*/
+    
+    /**
+     * @brief	ArUco dictionary of markers to be used.
+     *			See OpenCV's documentation on corresponding enum entries.
+     */
+    cv::aruco::PredefinedDictionaryType aruco_dict_;
+    cv::aruco::ArucoDetector detector_;		/**< ArUco marker detector.*/
+    
+    int marker_idx_;	/**< Index of ArUco marker to detect.*/
+    double B;			/**< Horizontal displacement of stereo cameras (assume no rotations).*/
     double f;			/**< Focal length.*/
+    double cx, cy;      /**< Center of left pinhole camera reference w.r.t. left image reference*/
 };
 
 #endif	// DETECTION_NODE_HPP_
--- aruco_loc/src/detection_node.cpp
+++ aruco_loc/src/detection_node.cpp
@@ -8,120 +8,133 @@ using std::placeholders::_2;
 
 
 ImageDetectionNode::ImageDetectionNode() : Node("detection_node") {
-	// Gets all potential parameters
-	this->declare_parameter("left_img_topic", "/left_image");
-	std::string left_img_topic = this->get_parameter("left_img_topic").as_string();
+    // Gets all potential parameters
+    this->declare_parameter("left_img_topic", "/left_image");
+    std::string left_img_topic = this->get_parameter("left_img_topic").as_string();
 
-	this->declare_parameter("right_img_topic", "/right_image");
-	std::string right_img_topic = this->get_parameter("right_img_topic").as_string();
-	
-	// Supposing horizontal displacement only between stereo images (value in cm)
-	this->declare_parameter("horizontal_d", 119.994);
-	B = this->get_parameter("horizontal_d").as_double();
+    this->declare_parameter("right_img_topic", "/right_image");
+    std::string right_img_topic = this->get_parameter("right_img_topic").as_string();
+    
+    // Supposing horizontal displacement only between stereo images (value in cm)
+    this->declare_parameter("horizontal_d", 119.994);
+    B = this->get_parameter("horizontal_d").as_double();
 
-	// Focal length (value in pixels)
-	this->declare_parameter("focal_length", 699.74);
-	f = this->get_parameter("focal_length").as_double();
+    // Focal length (value in pixels)
+    this->declare_parameter("focal_length", 699.74);
+    f = this->get_parameter("focal_length").as_double();
 
-	// Parameters for aruco detector
-	this->declare_parameter("marker_idx", 16);
-	marker_idx_ = this->get_parameter("marker_idx").as_int();
+    // Left camera coordinate centers w.r.t. image reference
+    this->declare_parameter<double>("center_x", 642.775);
+    cx = this->get_parameter("center_x").as_double();
 
-	this->declare_parameter("aruco_dict", static_cast<int>(cv::aruco::DICT_6X6_250));
-	aruco_dict_ = static_cast<cv::aruco::PredefinedDictionaryType>(this->get_parameter("aruco_dict").as_int());
+    this->declare_parameter<double>("center_y", 358.576);
+    cy = this->get_parameter("center_y").as_double();
 
-	// Defines quality of service: all messages that you want to receive must have the same
-	rclcpp::QoS custom_qos_profile = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default))
-	.history(rmw_qos_history_policy_t::RMW_QOS_POLICY_HISTORY_KEEP_LAST)
-	.keep_last(10)
-	.reliability(rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
-	.durability(rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE)
-	.avoid_ros_namespace_conventions(false);
+    // Parameters for aruco detector
+    this->declare_parameter("marker_idx", 16);
+    marker_idx_ = this->get_parameter("marker_idx").as_int();
 
-	// Subscribes to image topics
-	sub_right_img_.subscribe(this, right_img_topic, custom_qos_profile.get_rmw_qos_profile());
-	sub_left_img_.subscribe(this, left_img_topic, custom_qos_profile.get_rmw_qos_profile());
+    this->declare_parameter("aruco_dict", static_cast<int>(cv::aruco::DICT_6X6_250));
+    aruco_dict_ = static_cast<cv::aruco::PredefinedDictionaryType>(this->get_parameter("aruco_dict").as_int());
 
-	// Creates exact synchronization based on timestamp data in message headers
-	sync_exact_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image,sensor_msgs::msg::Image>>(sub_left_img_, sub_right_img_, 10);
-	auto Func = std::bind(&ImageDetectionNode::exact_sync_callback, this, _1, _2);
-	sync_exact_->registerCallback(Func);
+    // Defines quality of service: all messages that you want to receive must have the same
+    rclcpp::QoS custom_qos_profile = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default))
+    .history(rmw_qos_history_policy_t::RMW_QOS_POLICY_HISTORY_KEEP_LAST)
+    .keep_last(10)
+    .reliability(rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
+    .durability(rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE)
+    .avoid_ros_namespace_conventions(false);
 
-	// create publisher for target location point
-	pub_ = this->create_publisher<geometry_msgs::msg::Point>("/target_loc", 10);
-	
-	// Selects dict of markers to use
-	cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(aruco_dict_);
+    // Subscribes to image topics
+    sub_right_img_.subscribe(this, right_img_topic, custom_qos_profile.get_rmw_qos_profile());
+    sub_left_img_.subscribe(this, left_img_topic, custom_qos_profile.get_rmw_qos_profile());
 
-	// Leaving detection parameters as defaults
-	detector_ = cv::aruco::ArucoDetector(dictionary);
+    // Creates exact synchronization based on timestamp data in message headers
+    sync_exact_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image,sensor_msgs::msg::Image>>(sub_left_img_, sub_right_img_, 10);
+    auto Func = std::bind(&ImageDetectionNode::exact_sync_callback, this, _1, _2);
+    sync_exact_->registerCallback(Func);
+
+    // create publisher for target location point
+    pub_ = this->create_publisher<geometry_msgs::msg::PointStamped>("/target_loc", custom_qos_profile);
+    
+    // Selects dict of markers to use
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(aruco_dict_);
+
+    // Leaving detection parameters as defaults
+    detector_ = cv::aruco::ArucoDetector(dictionary);
 }
 
 
 void ImageDetectionNode::exact_sync_callback(const sensor_msgs::msg::Image::ConstSharedPtr left_img, const sensor_msgs::msg::Image::ConstSharedPtr right_img) {
-	RCLCPP_INFO(this->get_logger(), "Received synchronized stereo images.");
-	stereo_target_detect(cv_bridge::toCvShare(left_img), cv_bridge::toCvShare(right_img));
+    RCLCPP_INFO(this->get_logger(), "Received synchronized stereo images.");
+    stereo_target_detect(cv_bridge::toCvShare(left_img), cv_bridge::toCvShare(right_img));
 }
 
 
 void ImageDetectionNode::stereo_target_detect(cv_bridge::CvImageConstPtr left_img, cv_bridge::CvImageConstPtr right_img) {
-	// Contains the ids of markers found each image
-	std::vector<int> left_markerIds;
-	std::vector<int> right_markerIds;
-	// Corner location of found markers. order is based on list of ids
-	std::vector<std::vector<cv::Point2f>> left_markerCorners;
-	std::vector<std::vector<cv::Point2f>> right_markerCorners;
-	cv::Mat left, right;
-	// We accept the standard color format from stereolabs cameras. detector accepts color images without alpha component.
-	cv::cvtColor(left_img->image, left, cv::COLOR_RGBA2RGB);
-	cv::cvtColor(right_img->image, right, cv::COLOR_BGRA2BGR);
+    // Contains the ids of markers found each image
+    std::vector<int> left_markerIds;
+    std::vector<int> right_markerIds;
+    // Corner location of found markers. order is based on list of ids
+    std::vector<std::vector<cv::Point2f>> left_markerCorners;
+    std::vector<std::vector<cv::Point2f>> right_markerCorners;
+    cv::Mat left, right;
+    // We accept the standard color format from stereolabs cameras. detector accepts color images without alpha component.
+    cv::cvtColor(left_img->image, left, cv::COLOR_RGBA2RGB);
+    cv::cvtColor(right_img->image, right, cv::COLOR_BGRA2BGR);
 
-	detector_.detectMarkers(left, left_markerCorners, left_markerIds);
-	detector_.detectMarkers(right, right_markerCorners, right_markerIds);
-	// Look for our marker. if not found, do not proceed further!
-	auto left_mark_it = std::find(left_markerIds.begin(), left_markerIds.end(), marker_idx_);
-	auto right_mark_it = std::find(right_markerIds.begin(), right_markerIds.end(), marker_idx_);
-	if (left_mark_it == left_markerIds.end()) {
-		RCLCPP_WARN(this->get_logger(), "No marker found in left image!");
-	}
-	else if (right_mark_it == right_markerIds.end()) {
-		RCLCPP_WARN(this->get_logger(), "No marker found in right image!");
-	}
-	else {
-		cv::Point2f left_center = target_center(left_markerCorners[std::distance(left_markerIds.begin(), left_mark_it)]);
-		cv::Point2f right_center = target_center(right_markerCorners[std::distance(right_markerIds.begin(), right_mark_it)]);
-		
-		geometry_msgs::msg::Point pt_msg;
-		pt_msg.x = left_center.x;
-		pt_msg.y = left_center.y;
-		
-		// Depth computation assumes no rotation (otherwise epipolar geometry is needed)
-		pt_msg.z = B*f / (std::abs(left_center.x - right_center.x));
-		RCLCPP_INFO(this->get_logger(), "Marker location found!");
-		pub_->publish(pt_msg);
-	}
+    detector_.detectMarkers(left, left_markerCorners, left_markerIds);
+    detector_.detectMarkers(right, right_markerCorners, right_markerIds);
+    // Look for our marker. if not found, do not proceed further!
+    auto left_mark_it = std::find(left_markerIds.begin(), left_markerIds.end(), marker_idx_);
+    auto right_mark_it = std::find(right_markerIds.begin(), right_markerIds.end(), marker_idx_);
+    if (left_mark_it == left_markerIds.end()) {
+        RCLCPP_WARN(this->get_logger(), "No marker found in left image!");
+    }
+    else if (right_mark_it == right_markerIds.end()) {
+        RCLCPP_WARN(this->get_logger(), "No marker found in right image!");
+    }
+    else {
+        cv::Point2f left_center = target_center(left_markerCorners[std::distance(left_markerIds.begin(), left_mark_it)]);
+        cv::Point2f right_center = target_center(right_markerCorners[std::distance(right_markerIds.begin(), right_mark_it)]);
+        
+        geometry_msgs::msg::PointStamped pt_msg;
+        // transform from millimeters to meters
+        // Depth computation assumes no rotation (otherwise epipolar geometry is needed)
+        // f is in pixels, so we do not need to convert image coordinates (that are in pixels)
+        double z = B*f / (std::abs(left_center.x - right_center.x) * 1000.);
+        pt_msg.point.x = ((left_center.x - this->cx) * z) / f;
+        pt_msg.point.y = ((left_center.y - this->cy) * z) / f;
+        pt_msg.point.z = z;
 
+        // Set point header
+        pt_msg.header.stamp = this->now();
+        pt_msg.header.frame_id = "camera_link";
+        
+        RCLCPP_INFO(this->get_logger(), "Marker location found!");
+        pub_->publish(pt_msg);
+    }
 }
 
 
 cv::Point2f ImageDetectionNode::target_center(std::vector<cv::Point2f> const &corners) const {
-	cv::Point2f center;
+    cv::Point2f center;
 
-	for (auto const &corner : corners) {
-		center.x += corner.x / 4;
-		center.y += corner.y / 4;
-	}
+    for (auto const &corner : corners) {
+        center.x += corner.x / 4;
+        center.y += corner.y / 4;
+    }
 
-	return center;
+    return center;
 }
 
 
 int main(int argc, char ** argv) {
-	rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-	auto node = std::make_shared<ImageDetectionNode>();
-	rclcpp::spin(node);
-	rclcpp::shutdown();
-	
-	return 0;
+    auto node = std::make_shared<ImageDetectionNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    
+    return 0;
 }
