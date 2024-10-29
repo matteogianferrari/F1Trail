7257f59a131968d515993dc311a3940f7f3a1dd2
MatteoBaccilieri <matteo.baccilieri@studio.unibo.it>
2024-10-29 17:17:43 +0100

    Changed launch file

--- pid_controller/launch/longitudinal_pid_launch.py
+++ pid_controller/launch/longitudinal_pid_launch.py
@@ -1,13 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+    config = os.path.join(get_package_share_directory('longitudinal_pid'), 'config', 'longitudinal_pid.yaml')
+
     longitudinal_pid = Node(
         package='longitudinal_pid',
         executable='longitudinal_pid_node',
-        name='longitudinal_pid'
+        name='longitudinal_pid',
+        parameters= [config]
     )
     
     ld.add_action(longitudinal_pid)
