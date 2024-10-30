5768b2d44567856f3ff2581d7be0e9fdc0aa744b
Matteo Baccilieri <matteo.baccilieri@studio.unibo.it>
2024-10-30 14:42:54 +0100

    First version of longitudinal pid ctr (#3)
    * First version of longitudinal pid ctr
    
    * Minor adjustments and tuning
    
    * Added header file
    
    * Update node for compatibility with tracker and some PID tuning
    
    * Documentation and code refactoring.
    
    * Changed threshold distance as ROS2 parameter instead of hard-coded.
    
    * Removed useless .gitignore file.
    
    * Changed launch file
    
    ---------
    
    Co-authored-by: matteogianferrari <mine.matt88@gmail.com>

---
+++ pid_controller/include/pid_controller/longitudinal_pid.hpp
@@ -0,0 +1,145 @@
+/**
+ * @file    longitudinal_pid.hpp
+ * 
+ * @author  Matteo Baccilieri
+ *
+ * @date    2024-10-22
+ * 
+ * @brief   Header file for Longitudinal PID Controller.
+ */
+#ifndef LONGITUDINAL_PID_HPP
+#define LONGITUDINAL_PID_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include <chrono>
+#include <memory>
+#include <cmath>
+
+/**
+ * @class   LongitudinalPIDNode
+ * 
+ * @brief   Implements a PID controller for longitudinal control of a vehicle.
+ *
+ * @details This node subscribes to odometry and target position topics, calculates the 
+ *          control effort using a PID controller to move the vehicle towards the target position, 
+ *          and publishes throttle commands to control the vehicle's speed.
+ */
+class LongitudinalPIDNode : public rclcpp::Node
+{
+public:
+    /**
+     * @fn      LongitudinalPIDNode
+     *
+     * @brief   Constructs a new LongitudinalPIDNode object.
+     */
+    LongitudinalPIDNode();
+
+private:
+    /**
+     * @struct  Position
+     * 
+     * @brief   A simple struct to represent the x and y coordinates of the vehicle and target positions.
+     */
+    struct Position {
+        float x;    /**< X coordinate.*/
+        float y;    /**< Y coordinate.*/
+    };
+
+    /**
+     * @fn          odom_callback
+     * 
+     * @brief       Callback function for the odometry data.
+     *
+     * @details     Whenever new odometry data is received, it updates the
+     *              current position and speed of the vehicle.
+     * 
+     * @param[in]   msg A shared pointer to the received Odometry message.
+     */
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+    /**
+     * @fn          target_position_callback
+     * 
+     * @brief       Callback function for the target position.
+     *
+     * @details     When a new target position is received, it updates the target position that the vehicle 
+     *              should move towards.
+     * 
+     * @param[in]   msg_loc A shared pointer to the received Point message with target coordinates.
+     */
+    void target_position_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg_loc);
+
+    /**
+     * @fn      control_loop
+     * 
+     * @brief   Control loop for periodically updating throttle commands.
+     *
+     * @details It calculates the distance to the target, updates the PID controller, and publishes 
+     *          a throttle command based on the control effort. 
+     */
+    void control_loop();
+
+    /**
+     * @fn          update_pid
+     *
+     * @brief       Updates the PID controller output.
+     *
+     * @details     Calculates the PID output (throttle) based on the distance to the target position and 
+     *              current speed of the vehicle.
+     *
+     * @param[in]   distance_to_target The current distance to the target position.
+     * @param[in]   current_speed The current speed of the vehicle.
+     *
+     * @return      float The PID-calculated throttle value.
+     */
+    float update_pid(float distance_to_target, float current_speed);
+    
+
+    /**
+     * @brief   Subscriber for odometry data.
+     */
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+    /**
+     * @brief   Subscriber for target position data.
+     */
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr target_position_sub;
+    /**
+     * @brief   Publisher for throttle commands.
+     */
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr throttle_pub;
+    
+    float Kp;   /**< Proportional gain for the PID controller.*/
+    float Ki;   /**< Integral gain for the PID controller.*/
+    float Kd;   /**< Derivative gain for the PID controller.*/
+
+    float tau;  /**< Time constant for the PID controller filter.*/
+
+    float delta_time;   /**< Time interval for updating the PID controller.*/
+
+    double threshold_distance;  /**< Threshold distance to maintain from the tracked object.*/
+
+    float integrator_min;   /**< Minimum value for the integrator to avoid windup.*/
+    float integrator_max;   /**< Maximum value for the integrator to avoid windup.*/
+    float integrator;       /**< Integrator state for the PID controller.*/
+
+    float prev_error;       /**< The previous error value for calculating derivative.*/
+    float differentiator;   /**< Differentiator state for the PID controller.*/
+    
+    float prev_measurement; /**< Previous measurement for filtering.*/
+    
+    float pid_output;   /**< Output from the PID controller.*/
+
+    float current_speed;    /**< Current speed of the vehicle.*/
+
+    Position current_position;  /**< Current position of the vehicle.*/
+    Position target_position;   /**< Target position the vehicle should reach.*/
+
+    rclcpp::TimerBase::SharedPtr control_timer; /**< Timer for periodically running the control loop.*/
+};
+
+#endif  // LONGITUDINAL_PID_HPP_
---
+++ pid_controller/src/longitudinal_pid.cpp
@@ -0,0 +1,124 @@
+#include "longitudinal_pid.hpp"
+
+
+// Used for parameter binding in callbacks to be registered with subscribers.
+using std::placeholders::_1;
+
+
+LongitudinalPIDNode::LongitudinalPIDNode() : Node("longitudinal_ctr"),
+    Kp(0.2f), Ki(0.01f), Kd(0.15f),
+    integrator(0.0f), integrator_min(-0.4f), integrator_max(5.0f),
+    prev_error(0.0f), differentiator(0.0f), prev_measurement(0.0f),
+    pid_output(0.0f), current_speed(0.0f), delta_time(0.5f),
+    current_position{0.0f, 0.0f}, target_position{0.0f, 0.0f}           
+{
+    // Gets all potential parameters
+	this->declare_parameter("threshold_distance", 2.0);
+	threshold_distance = this->get_parameter("threshold_distance").as_double();
+
+    // Defines quality of service: all messages that you want to receive must have the same
+	rclcpp::QoS custom_qos_profile = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default))
+	.history(rmw_qos_history_policy_t::RMW_QOS_POLICY_HISTORY_KEEP_LAST)
+	.keep_last(10)
+	.reliability(rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
+	.durability(rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE)
+	.avoid_ros_namespace_conventions(false);
+
+    // Subscribes to odometry to track car's position
+    odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "/ego_racecar/odom", 10, std::bind(&LongitudinalPIDNode::odom_callback, this, std::placeholders::_1));
+
+    // Subscribes to target position messages
+    target_position_sub = this->create_subscription<geometry_msgs::msg::PointStamped>(
+        "/tracked_obj_loc", custom_qos_profile, std::bind(&LongitudinalPIDNode::target_position_callback, this, std::placeholders::_1));
+
+    // Publisher for throttle command
+    throttle_pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("/drive", 10);
+}
+
+
+void LongitudinalPIDNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+    // Updates the current position and speed of the vehicle
+    current_position.x = msg->pose.pose.position.x; 
+    current_position.y = msg->pose.pose.position.y; 
+    current_speed = msg->twist.twist.linear.x;
+}
+
+
+void LongitudinalPIDNode::target_position_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg_tracker) {
+    // Updates the target position
+    target_position.x = msg_tracker->point.x; 
+    target_position.y = msg_tracker->point.y;
+
+    // Executes the PID control loop
+    control_loop();
+}
+
+
+void LongitudinalPIDNode::control_loop() {
+    // Computes the distance to the target position
+    float distance = sqrt(pow((target_position.x - current_position.x), 2) +
+                          pow((target_position.y - current_position.y), 2));
+
+    // Updates PID controller with the current distance to the target
+    float throttle = update_pid(distance, current_speed);
+
+    // Creates and publish throttle command
+    ackermann_msgs::msg::AckermannDriveStamped drive_msg;
+    drive_msg.drive.speed = throttle; 
+    throttle_pub->publish(drive_msg); 
+}
+
+
+float LongitudinalPIDNode::update_pid(float distance_to_target, float current_speed) {
+    // Checks if the target distance is lower than the threshold
+    if (distance_to_target < threshold_distance) {
+        RCLCPP_INFO(this->get_logger(), "Car is near the target, stopping %f", distance_to_target);
+
+        // Stops the car (throttle = 0) and resets the integrator to avoid windup
+        pid_output = 0.0f;
+        integrator = 0.0f;
+
+        return pid_output;
+    }
+
+    // Calculate the error (distance to target)
+    float error = distance_to_target;
+
+    // Proportional term
+    float proportional = Kp * error;
+
+    // Integral term with anti-windup using the trapezoidal rule
+    integrator += 0.5f * Ki * delta_time * (error + prev_error);
+
+    // Clamp integrator to its min and max values
+    if (integrator > integrator_max) {
+        integrator = integrator_max;
+    }
+    else if (integrator < integrator_min) {
+        integrator = integrator_min;
+    }
+
+    // Derivative term (based on change in error over time)
+    differentiator = Kd * (error - prev_error) / delta_time;
+
+    // Compute final PID output (throttle) and apply limits.
+    // Subtracting current speed for smooth control.
+    pid_output = proportional + integrator + differentiator - current_speed;
+
+    // Stores the current error for the next update
+    prev_error = error;
+
+    return pid_output;
+}
+
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<LongitudinalPIDNode>();
+    rclcpp::spin(node);
+
+    rclcpp::shutdown();
+    return 0;
+}
